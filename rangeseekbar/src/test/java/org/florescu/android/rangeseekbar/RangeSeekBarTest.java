ad6fbf84e031958199039cf8ac8bb3cfa14cabdb
Alex Florescu <anothem@gmail.com>
2016-12-02 14:17:24 +0000

    Add empty test

--- rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
+++ rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
@@ -1,12 +1,19 @@
 package org.florescu.android.rangeseekbar;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class RangeSeekBarTest {
 
-
+    @Test
+    public void emptyTest() {
+        // TODO
+        assertThat(true).isTrue();
+    }
 }
